0457149dc28d46dc5f2486e684dce037506f8a2d
danialma3 <danialma3@gmail.com>
2023-02-05 00:08:43 +0700

    frist commit

---
+++ resources/views/assets/e_artists.blade.php
@@ -0,0 +1,103 @@
+<section class="artists-section section-padding" id="section_3">
+            <div class="container">
+                <div class="row justify-content-center">
+
+                    <div class="col-12 text-center">
+                        <h2 class="mb-4">Guest Stars</h1>
+                    </div>
+
+                    <div class="col-lg-5 col-12">
+                        <div class="artists-thumb">
+                            <div class="artists-image-wrap">
+                                <img src="{{asset('assets/tema')}}/images/artists/cellozs.jpg" class="artists-image img-fluid">
+                            </div>
+
+                            <div class="artists-hover">
+                                <p>
+                                    <strong>Nick:</strong>
+                                    Yoshua Marcellos
+                                </p>
+
+                                <p>
+                                    <strong>TTL:</strong>
+                                    10 September 1999
+                                </p>
+
+                                <p>
+                                    <strong>IG:</strong>
+                                    @celloszxz
+                                </p>
+
+                                <hr>
+
+                                <p class="mb-0">
+                                    <strong>Youtube Channel:</strong>
+                                    <a href="https://www.youtube.com/@celloszxz">celloszxz</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-5 col-12">
+                        <div class="artists-thumb">
+                            <div class="artists-image-wrap">
+                                <img src="{{asset('assets/tema')}}/images/artists/fadil_jaidi.webp" class="artists-image img-fluid">
+                            </div>
+
+                            <div class="artists-hover">
+                                <p>
+                                    <strong>Nama:</strong>
+                                    Fadil Jaidi
+                                </p>
+
+                                <p>
+                                    <strong>TTL:</strong>
+                                    17 Oktober 1994
+                                </p>
+
+                                <p>
+                                    <strong>IG:</strong>
+                                    fadiljaidi
+                                </p>
+
+                                <hr>
+
+                                <p class="mb-0">
+                                    <strong>Youtube Channel:</strong>
+                                    <a href="https://www.youtube.com/@fadiljaidi7416">Fadil Jaidi</a>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="artists-thumb">
+                            <img src="{{asset('assets/tema')}}/images/artists/sarah_viloid.jpg" class="artists-image img-fluid">
+
+                            <div class="artists-hover">
+                                <p>
+                                    <strong>Nama:</strong>
+                                    Sarah Olivia S.
+                                </p>
+
+                                <p>
+                                    <strong>TTL:</strong>
+                                    16 September 2000
+                                </p>
+
+                                <p>
+                                    <strong>IG:</strong>
+                                    sarahvilo.id
+                                </p>
+
+                                <hr>
+
+                                <p class="mb-0">
+                                    <strong>Youtube Channel:</strong>
+                                    <a href="https://www.youtube.com/@SarahViloid">Sarah Viloid</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
