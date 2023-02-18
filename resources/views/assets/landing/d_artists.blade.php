eb599c22ab13f371f80a7ab4eacbfaad9dcf2560
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-02-18 18:55:15 +0800

    add responsive iphone SE

--- resources/views/assets/landing/d_artists.blade.php
+++ resources/views/assets/landing/d_artists.blade.php
@@ -1,9 +1,9 @@
-<section class="artists-section section-padding" id="section_2">
+<section class="artists-section section-padding" id="section_2" style="border: transparent">
     <div class="container text-center text-white">
         <h2 class="text-white">Guest Stars</h2>
         <p class="text-white">National & Local Talent</p>
         <div class="d-flex justify-content-center flex-wrap">
-            <div class="card border-0 guest-description">
+            <div class="card border-0 guest-description" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
                 <img src="{{ asset('assets/tema') }}/images/artists/GS-AUDI-MAGUS.png" type="png" poster="">
                 <img class="star-icon" src="{{asset('assets/tema')}}/images/template/star-icon.png" type="png" poster="">
 
@@ -12,13 +12,13 @@
                     </p>
                 </div>
             </div>
-            <div class="card border-0 guest-description">
+            <div class="card border-0 guest-description" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
                 <img src="{{ asset('assets/tema') }}/images/artists/GS-UNKNOWN.png" type="png" poster="">
                 <div class="card-body">
                     <p class="text-white">Main Guest</p>
                 </div>
             </div>
-            <div class="card border-0 guest-description">
+            <div class="card border-0 guest-description" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
                 <img src="{{ asset('assets/tema') }}/images/artists/GS-COSPLAYER.png" type="png" poster="">
                 <div class="card-body">
                     <p class="text-white">Cosplayers</p>
