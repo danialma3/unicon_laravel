39682e1c7c8977cc9a3d93193da41f6d2e8c54b0
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-02-18 16:44:48 +0800

    add mushroom icon

--- resources/views/assets/landing/e_sneakpeak.blade.php
+++ resources/views/assets/landing/e_sneakpeak.blade.php
@@ -6,6 +6,8 @@
             <div class="card border-0 sneak-peak-image">
                 <img class="event_highlight" src="{{asset('assets/tema')}}/images/template/EVENT_HIGHLIGHT.png" type="png" poster="">
                 <img class="tanda-tanya-icon" src="{{asset('assets/tema')}}/images/template/tanda-tanya-icon.png" type="png" poster="">
+                <img class="mushroom-icon" src="{{asset('assets/tema')}}/images/template/mushroom-icon.png" type="png" poster="">
+
             </div>
         </div>
     </section>
--- resources/views/assets/landing/f_eventhub.blade.php
+++ resources/views/assets/landing/f_eventhub.blade.php
@@ -1,4 +1,3 @@
-
 <div class="blank-space-triangle"></div>
 <div class="triangle-left-2"></div>
 
@@ -6,7 +5,7 @@
 <div class="triangle-right"></div>
 
 <section class="eventhub-section " id="section_4">
-    <div class="container text-center" >
+    <div class="container text-center">
         <h2 class="text-white">Events Hub</h2>
         <p class="text-white ">Road to Event Project You Might Love</p>
         <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
@@ -19,12 +18,13 @@
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
-                <span class="arrow-left"  aria-hidden="true"></span>
+                <span class="arrow-left" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next"  type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
                 <span class="arrow-right" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
+    </div>
 </section>
