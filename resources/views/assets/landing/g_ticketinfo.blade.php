9d8fef3a152d807ed01c2fe9ed0774f348a80871
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-02-19 15:12:19 +0800

    fix/carousel padding

--- resources/views/assets/landing/g_ticketinfo.blade.php
+++ resources/views/assets/landing/g_ticketinfo.blade.php
@@ -1,20 +1,21 @@
-<section class="ticketinfo-section border-0" id="section_5"  >
-    <div class="container" >
+<section class="ticketinfo-section border-0" id="section_5">
+    <div class="container">
         <div class="row row-cols-1 row-cols-md-2 g-4">
             <div class="col co;">
-                <div class="card bg-transparent border-0 hand-band" >
+                <div class="card bg-transparent border-0 hand-band">
                     <img src="{{asset('assets/tema')}}/images/template/Paper_Wristbands_Mockup_01.png" type="png" poster="">
                 </div>
             </div>
             <div class="col">
                 <div class="card bg-transparent border-0">
                     <div class="card-body bg-transparent countdown-container">
-                        <h2 class="text-white" >Ticket <br>Presales</h2>
-                        <p class="text-white" >opening in</p>
+
+                        <h2 class="text-white">Ticket <br>Presales</h2>
+                        <p class="text-white">opening in</p>
                         <div id="countdown" style="width: fit-content;">
                             <div class=" d-flex align-items-end">
                                 <p class="days"><span id="days"></span></p>
-                                <p class="text-days">  Days  </p>
+                                <p class="text-days"> Days </p>
                                 <p class="hours"><span id="hours"></span></p>
                                 <p class="petik"><span id="petik">:</span></p>
                                 <p class="minutes"><span id="minutes"></span></p>
