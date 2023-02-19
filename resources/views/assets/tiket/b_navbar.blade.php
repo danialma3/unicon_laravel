5cfc10e9c3cc7e1c0e2052159d2a5016955eabe7
Muhammad Khuzain <mkhuzain17@gmail.com>
2023-02-19 15:52:20 +0800

    fix/footer and remove unused files from tiket view

--- resources/views/assets/tiket/b_navbar.blade.php
+++ resources/views/assets/tiket/b_navbar.blade.php
@@ -2,21 +2,19 @@
 
     <nav class="navbar navbar-expand-lg">
         <div class=" container">
-            <a class="navbar-brand logo-unicon" href="/tiket">
-                <img src="{{asset('assets/tema')}}/images/uniconweb.png?07022023.001" type="png" poster="">
-            </a>
+            <div class="d-flex flex-row">
+                <a class="navbar-brand logo-unicon" href="/tiket">
+                    <img src="{{asset('assets/tema')}}/images/uniconweb.png?07022023.001" type="png" poster="">
+                </a>
 
-            <p class="navbar-brand text-by">by</p>
+                <p class="navbar-brand text-by">by</p>
 
-            <a class="navbar-brand logo-kalsel-unite" href="/tiket">
-                <img src="{{asset('assets/tema')}}/images/kalselunite.png?07022023.001" type="png" poster="">
-            </a>
+                <a class="navbar-brand logo-kalsel-unite" href="/tiket">
+                    <img src="{{asset('assets/tema')}}/images/kalselunite.png?07022023.001" type="png" poster="">
+                </a>
+            </div>
 
-            <a href="/" class="btn custom-btn d-lg-none ms-auto me-4">Kembali</a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <a href="/" class="btn custom-btn d-lg-none ms-auto me-4 border-0">Kembali</a>
 
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav align-items-lg-center ms-auto me-lg-5">
